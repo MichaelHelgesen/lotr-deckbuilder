338b30cad2ca126c34d7ec7e972cfcfcbe5cfd01
Michael Helgesen <michaelhelgesen02@gmail.com>
2026-07-08 16:07:51 +0200

    Add constructor to Card class

--- Card.cs
+++ Card.cs
@@ -5,4 +5,12 @@ public class Card
     public int Threat { get; set; }
     public int Willpower { get; set; }
     public string Type { get; set; }
+    public Card(string title, string sphere, int threat, int willpower, string type)
+   {
+      this.Title = title;
+      this.Sphere = sphere;
+      this.Threat = threat;
+      this.Willpower = willpower;
+      this.Type = type;
+   }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿
+
+List<Card> deck = new List<Card>();
+
+deck.Add(new Card(title: "Aragorn", sphere: "Leadership", threat: 12, willpower: 3, type: "Hero"));
+deck.Add(new Card(title: "Gandalf", sphere: "Lore", threat: 3, willpower: 3, type: "Ally"));
+
+foreach (Card card in deck)
+{
+    Console.WriteLine(card.Title);
+}
+
+deck.Remove(deck[0]); // fjerner første kort
+Console.WriteLine(deck.Count); // antall kort i listen
+
+
