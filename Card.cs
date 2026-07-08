58b39e8b25b387edc4bae984902296c50e422f67
Michael Helgesen <michaelhelgesen02@gmail.com>
2026-07-08 14:45:14 +0200

    first commit

---
+++ Card.cs
@@ -0,0 +1,8 @@
+public class Card
+{
+    public string Title { get; set; }
+    public string Sphere { get; set; }
+    public int Threat { get; set; }
+    public int Willpower { get; set; }
+    public string Type { get; set; }
+}
